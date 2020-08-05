85567c95aed92e8aac39d67f0e9a9cacc1c1a8a4
kshitij <kshitijagrawal2607@gmail.com>
2020-08-06 01:53:47 +0530

    Use TypeScript in Components

--- src/js/components/Home.tsx
+++ src/js/components/Home.tsx
@@ -1,24 +1,18 @@
-import React, { Component } from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 
-class Home extends Component {
-  render() {
+export const Home : React.FC = () => {
     return (
       <div>
-        <center>
           <h1>
             Kshitij Agrawal
           </h1>
           <h3>
             Work under progress. Please check out later
           </h3>
-        </center>  
       </div>
-    );
-  }
+    )
 }
-
-export default Home;
 
 const wrapper = document.getElementById("container");
 wrapper ? ReactDOM.render(<Home />, wrapper) : false;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
+  resolve: {
+    // look for these file extensions
+    extensions: [".ts", ".tsx", ".js"],
+  },
   module: {
     rules: [
+      // babel-loader to load our jsx and tsx files
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
+      },
+      {
+        // css-loader to bundle all the css files into one file
+        // style-loader to add all the styles  inside the style tag of the document
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
@@ -19,6 +31,9 @@ module.exports = {
         ]
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: true, // Helpful to avoid 404s in development
   },
   plugins: [
     new HtmlWebPackPlugin({
